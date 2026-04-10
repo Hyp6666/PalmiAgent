a1243738c701897ae3ecc9ac2d310fc4657be46e
Hyp6666 <hongyupeng72@gmail.com>
2026-04-11 00:53:09 +0800

    Initial Commit

---
+++ PalmiAgent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PalmiAgent
+//
+//  Created by 洪昱鹏 on 2026/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PalmiAgent/PalmiAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalmiAgentApp.swift
+//  PalmiAgent
+//
+//  Created by 洪昱鹏 on 2026/4/11.
+//
+
+import SwiftUI
+
+@main
+struct PalmiAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
